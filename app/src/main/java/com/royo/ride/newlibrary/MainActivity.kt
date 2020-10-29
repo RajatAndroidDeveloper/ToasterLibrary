499ff2ef2034cccd8b6e9947f4b5433cc8249fe9
Rajat Tyagi <rajat.tyagi@codebrewinnovations.com>
2020-10-29 11:06:39 +0530

    latest code

---
+++ app/src/main/java/com/royo/ride/newlibrary/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.royo.ride.newlibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.royo.ride.mylibrary2.MainActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
